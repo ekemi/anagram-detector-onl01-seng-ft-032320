1e84a8a6ff2e6f73d469f25e5337e7292c806a08
edmundo papi <edmundopierre@yahoo.com>
2020-04-08 13:59:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+   @word = word
+
+  end
+ def match(array)
+   array.each do | value|
+     array_split = value.split("")
+     if array_split.sort == @word.split("").sort
+              value
+      else
+        return []
+            end
+
+  end
+ end
+end
